d6d996fc7c21d6ba05f8789afbdb6435277d2cd0
HQendresa <qendresa.hoxha@student.uni-pr.edu>
2023-01-27 18:35:33 +0100

    Implemented calculate horizontal value function and trace path function

--- AI_Detyra_2b/AI_Detyra_2b/Source.cpp
+++ AI_Detyra_2b/AI_Detyra_2b/Source.cpp
@@ -30,6 +30,38 @@ bool isDestination(const Pair& position, const Pair& dest)
 {
 	return position == dest;
 }
+
+double calculateHValue(const Pair& src, const Pair& dest)
+{
+	return sqrt(pow((src.first - dest.first), 2.0)
+		+ pow((src.second - dest.second), 2.0));
+}
+
+template <size_t ROW, size_t COL>
+void tracePath(const array<array<cell, COL>, ROW>& cellDetails, const Pair& dest)
+{
+	printf("\nThe Path is ");
+
+	stack<Pair> Path;
+
+	int row = dest.first;
+	int col = dest.second;
+	Pair next_node = cellDetails[row][col].parent;
+	do {
+		Path.push(next_node);
+		next_node = cellDetails[row][col].parent;
+		row = next_node.first;
+		col = next_node.second;
+	} while (cellDetails[row][col].parent != next_node);
+
+	Path.emplace(row, col);
+	while (!Path.empty()) {
+		Pair p = Path.top();
+		Path.pop();
+		printf("-> (%d,%d) ", p.first, p.second);
+	}
+}
+
 int main() {
 
 	cout << "Working fine" << endl;
