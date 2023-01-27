5fd4e30975687e8b6bc49a704190b6d8e6171ca8
HQendresa <qendresa.hoxha@student.uni-pr.edu>
2023-01-27 18:29:21 +0100

    Implementing the struct and declaring pairs and isValid function

--- AI_Detyra_2b/AI_Detyra_2b/Source.cpp
+++ AI_Detyra_2b/AI_Detyra_2b/Source.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <queue>
+typedef pair<int, int> Pair;
+typedef tuple<double, int, int> Tuple;
+
 using namespace std;
+struct cell {
+
+	Pair parent;
+
+	double f, g, h;
+	cell()
+		: parent(-1, -1)
+		, f(-1)
+		, g(-1)
+		, h(-1)
+	{
+	}
+};
 int main() {
 
 	cout << "Working fine" << endl;
